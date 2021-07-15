a10a052ab6523082b98d3858590edaf3d90638fe
jopemachine <jopemachine@naver.com>
2021-07-15 18:06:04 +0900

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+require('shelljs/global');
+const arvish = require('arvish');
+
+// exec shell silently
+config.silent = true;
+
+const info = exec('df -h').stdout;
+const lines = info.split('\n');
+const contents = lines.map(line => line.split(/\s+/));
+const diskinfo = contents.slice(1).filter(line => line.every(v => v !== '' && v !== undefined && v !== null));
+diskinfo.sort((a, b) => a[0].localeCompare(b[0]));
+const items = diskinfo.map(info => ({
+  title: `Size: ${info[1]}  Used: ${info[2]}  Avail: ${info[3]}`,
+  subtitle: info[0],
+  arg: `Size: ${info[1]}  Used: ${info[2]}  Avail: ${info[3]}`
+}));
+
+arvish.output(items);
---
+++ readme.md
@@ -0,0 +1,21 @@
+# arvis-diskusage
+
+> Show disk usage in Arvis
+
+![](./demo.png)
+
+## Install
+
+```
+$ npm install --global arvis-diskusage
+```
+
+
+## Usage
+
+In arvis, type `df`, <kbd>Enter</kbd>, and you'll see you mac disk usage info.
+
+
+## License
+
+MIT © [lcc19941214](http://achuan.me)
